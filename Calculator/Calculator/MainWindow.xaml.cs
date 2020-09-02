9e518a62cbca6c4304937ecfd5d89359a76720af
jdavalos1 <43660774+jdavalos1@users.noreply.github.com>
2020-09-02 14:48:05 -0700

    Basic number functionality added
    Whole numbers can be added to the textbox. Next is handle decimals as well as commas.

--- Calculator/Calculator/MainWindow.xaml.cs
+++ Calculator/Calculator/MainWindow.xaml.cs
@@ -20,9 +20,36 @@ namespace Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        private const string DEFAULTRESULTS = "0";
+        private const string NEGATIVERESULTS = "-";
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        public void NumbClick(object sender, RoutedEventArgs e)
+        {
+            var buttonClick = (sender as Button);
+
+            if (Results.Text.ToString().Equals(DEFAULTRESULTS))
+                Results.Text = buttonClick.Content.ToString();
+            else
+                Results.Text += buttonClick.Content.ToString();
+        }
+
+        public void ClearClick(object sender, RoutedEventArgs e)
+        {
+            Results.Text = DEFAULTRESULTS;
+        }
+
+        public void NegativeClick(object sender, RoutedEventArgs e)
+        {
+            if (Results.Text.Equals(DEFAULTRESULTS))
+                return;
+            if (Results.Text.Contains(NEGATIVERESULTS))
+                Results.Text = Results.Text.Remove(0,1);
+            else
+                Results.Text = Results.Text.Insert(0, NEGATIVERESULTS);
         }
     }
 }
